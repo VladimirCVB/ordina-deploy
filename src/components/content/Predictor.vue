6ec33c2c113d300b3ccec9738c01c1998eaccb25
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2021-06-07 17:36:26 +0300

    predictor and description

--- src/components/content/Predictor.vue
+++ src/components/content/Predictor.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
+  <div class="border-2 border-yellow-600 rounded-lg inline-block w-1/3 ml-24">
+      <!-- Predictor Title -->
+      <div class="text-white text-center text-2xl bg-yellow-600 shadow-lg">
+        <i class="fas fa-history mr-2 text-lg"></i>
+        <label>Predictor</label>
+      </div>
+
+      <!-- Content 'From' and 'To'   -->
+      <div class="p-5 divide-y divide-yellow-600 text-2xl">
+        <select name="cars" id="cars" class="block w-full outline-none">
+          <option value="" disabled selected>From</option>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+
+        <!-- <hr class="mt-2 mb-2 white text-white"/> -->
+
+        <select name="cars" id="cars" class="block w-full outline-none">
+          <option value="" disabled selected>To</option>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+
+      <!-- Departure Date Title -->
+      <div class="bg-yellow-600 text-white text-center text-2xl shadow-lg">
+        <i class="fas fa-train mr-2 text-lg"></i>
+        <label>Departure Date</label>
+      </div>
+      
+      <!-- Date Content -->
+      <div class="mt-5 mb-64 p-2 divide-y divide-yellow-500">
+        <input type="date" placeholder="Date" class="w-full outline-none text-xl" />
+        <hr />
+
+      </div>
+
+      <!-- Predict Button -->
+      <div class="text-center mt-5 mb-5">
+        <button class="bg-yellow-600 rounded-md text-white p-2 text-2xl w-5/6 shadow-lg">Make Prediction</button>
+      </div>
   </div>  
 </template>
 
--- src/views/Content.vue
+++ src/views/Content.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <Description class="float-left" />
+      <Description />
       <Predictor />
   </div>  
 </template>
