8ed6e35cfd02c2e7a6d0eb7cfdf751303df623e8
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2021-06-07 16:43:10 +0300

    created basic header

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-yellow-600 shadow-xl">
+      <header>
+          <div class="p-2 pl-10 text-white font-bold text-center inline-block">
+              <label class="text-3xl">Ordina</label><br />
+              <label class="text-sm">Delay Predictor</label>
+          </div>
+      </header>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+
+  }
+}
+</script>
