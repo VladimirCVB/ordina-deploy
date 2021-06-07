394b6f881c4c45a04dc2f04154af088fbab5b7a1
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2021-06-07 16:59:23 +0300

    created basic description + lorem ipsum

---
+++ src/components/content/Description.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-1/3 m-10 ml-20">
+      <img src="../../../public/Train image.jpg" height="350" width="855" class="rounded-xl mb-5 shadow-lg"/>
+      <div class="bg-yellow-600 p-10 pt-5 shadow-xl rounded-xl">
+          <h2 class="text-white font-bold text-center mb-5 text-xl">Ordina Delay Predictor</h2>
+          <p class="text-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam urna dolor, convallis eu libero a, mattis viverra metus. Proin vel est in orci ultrices suscipit et quis purus. Phasellus purus nibh, ultricies vitae orci non, viverra consequat ex. Proin feugiat iaculis orci, a dignissim est laoreet eu. Donec scelerisque, nunc in malesuada laoreet, purus lorem pretium velit, a consectetur mi nibh et dolor. Suspendisse bibendum semper nisi. Phasellus fermentum mi vitae nunc laoreet, ut varius ligula rutrum. Pellentesque tincidunt dolor sit amet nisi interdum tincidunt. Sed faucibus lorem vel enim rhoncus aliquet.</p>
+      </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Description',
+  props: {
+  }
+}
+</script>
---
+++ src/views/Content.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <Description class="float-left" />
+      <Predictor />
+  </div>  
+</template>
+
+<script>
+import Description from '../components/content/Description.vue'
+import Predictor from '../components/content/Predictor.vue'
+
+export default {
+  name: 'Content',
+  props: {
+      
+  },
+  components: {
+    Description,
+    Predictor
+  }
+}
+</script>
