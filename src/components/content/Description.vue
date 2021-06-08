b34aeb1620ace70a90ea150b9edd5ed2edc1ce34
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2021-06-08 10:32:08 +0300

    real description

--- src/components/content/Description.vue
+++ src/components/content/Description.vue
@@ -3,7 +3,7 @@
       <img src="../../../public/Train image.jpg" height="350" width="855" class="rounded-xl mb-5 shadow-lg"/>
       <div class="bg-yellow-600 p-10 pt-5 shadow-xl rounded-xl">
           <h2 class="text-white font-bold text-center mb-5 text-xl">Ordina Delay Predictor</h2>
-          <p class="text-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam urna dolor, convallis eu libero a, mattis viverra metus. Proin vel est in orci ultrices suscipit et quis purus. Phasellus purus nibh, ultricies vitae orci non, viverra consequat ex. Proin feugiat iaculis orci, a dignissim est laoreet eu. Donec scelerisque, nunc in malesuada laoreet, purus lorem pretium velit, a consectetur mi nibh et dolor. Suspendisse bibendum semper nisi. Phasellus fermentum mi vitae nunc laoreet, ut varius ligula rutrum. Pellentesque tincidunt dolor sit amet nisi interdum tincidunt. Sed faucibus lorem vel enim rhoncus aliquet.</p>
+          <p class="text-white text-justify">The Ordina Delay Predictor is a software that uses Artificial Intelligence to predict whether there will be a delay or not on the selected route. It uses national train data of the Netherlands and includes the five most important cities in the country, Groningen, Den Haag, Amsterdam, Utrecht and Eindhoven. It is simple to use, at it requires only the cities of departure and arrival and the date of travel, and based on the past year data it will predict whether there is a high change or low change of a delay at that time on that route.</p>
       </div>
   </div>  
 </template>
